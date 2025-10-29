fcf370e643750914d31ffa0875b4225d6cb56999
Cem Subaşı <cemsubasi@live.com>
2025-10-29 04:13:42 +0300

    Refactor[ab]: Run requests sequentially instead of background

--- ab/ab.sh
+++ ab/ab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ab -n 10000 -c 100 -p buy_order.json -T 'application/json' http://localhost:8080/orders &
-ab -n 10000 -c 100 -p sell_order.json -T 'application/json' http://localhost:8080/orders &
-ab -n 10000 -c 100 -p trade.json -T 'application/json' http://localhost:8080/orders &
+ab -n 10000 -c 100 -p buy.json -T 'application/json' http://localhost:8080/orders
+ab -n 10000 -c 100 -p sell.json -T 'application/json' http://localhost:8080/orders
+ab -n 10000 -c 100 -p trade.json -T 'application/json' http://localhost:8080/orders
